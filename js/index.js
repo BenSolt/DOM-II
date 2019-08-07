d88c6e437dc4f7101c4b1b887bee917206c15ca5
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-08-06 23:05:30 -0400

    got 10 unique event listeners and propagation and prevent default

--- js/index.js
+++ js/index.js
@@ -9,29 +9,68 @@
 
 
 
-
+// 1. Mouse Over
 
 const logo = document.querySelector('.logo-heading');
 
   logo.addEventListener('mouseover', (e) => {
     TweenMax.to(e.target, 1, {scale: 1.3 });
+    
 });
+
   
+
+// 2. Mouse Leave
+
     logo.addEventListener('mouseleave', (e) => {
     TweenMax.to(e.target, .5, {scale: 1 });
 });
 
 
 
+     
+  
 
- //Mouse Dub Click turns blue.
+
+ //3. Mouse Click turns purple
+
+ const navbar3 = document.querySelector('.home .intro h2');
+ navbar3.addEventListener('click', (e) => {
+  navbar3.style.color = 'purple'})
+    
+
+
+
+
+//header change color
+
+const navig = document.querySelector('.nav-container ');
+navig.addEventListener('dblclick', (e) => {
+   e.target.style.backgroundColor = 'green'
+   
+  })
+   
+
+ //4. Mouse Dub Click turns blue.
 
  const navbar2 = document.querySelectorAll('.nav .nav-link');
- navbar2.forEach(anchor2 => {
+ navbar2.forEach(anchor2 => { 
+  anchor2.addEventListener('click' , (e) =>{
+  e.preventDefault();
+  anchor2.style.color = 'blue'
+  })
    anchor2.addEventListener('dblclick', (e) => {
-    anchor2.style.color = 'blue'})
+    e.stopPropagation();
+    
+    anchor2.style.backgroundColor = 'orange'
+  })
     
 }); 
+
+
+  
+
+
    
 //Mouse Over and Mouse Leave Nav Bar
 
@@ -45,15 +84,25 @@ const navbar = document.querySelectorAll('.nav .nav-link');
     anchor.style.color = 'black'})
      }); 
     
-    
-// KeyDown  
+
+
+
+//5. KeyDown  
   const body = document.querySelector('body');
      body.addEventListener('keydown' , (e) => {
-        body.style.backgroundColor = 'red'   //'#' + Math.floor(Math.random()*16777215).toString(16)';
+        body.style.backgroundColor = 'red'//'# + Math.floor(Math.random()*16777215).toString(16)'
      });
 
 
-// Wheel
+
+
+
+
+
+
+
+
+//6. Wheel
 
 const wheel = document.querySelector('p');
      wheel.addEventListener('wheel' , (e) => {
@@ -61,52 +110,42 @@ const wheel = document.querySelector('p');
      });
 
 
-//Drag/drop
+//7. focus
+
+// const focus = document.querySelector('a');
+//      focus.addEventListener('focus' , (e) => {
+//         e.target.style.fontSize = "35px";
+//      });
+
+
+
+//8. mouseout
+
+ const mouseout = document.querySelector('.img-content img');
+ mouseout.addEventListener('mouseout' , (e) => {
+  alert("mouse out!");
+      });
+
+
+//9. Key up
+
+
+     const drag = document.querySelector('.home .intro img');
+   drag.addEventListener('mouseup', (e) => {
+    alert("mouse up over photo bus!");  
+  }); 
+
+//10. select
+
+const mousemove = document.querySelector('.home .inverse-content');
+   mousemove.addEventListener('mousemove', (e) => {
+    alert("mouse move!");  
+  }); 
 
 
 
 
-//load
-
-//focus
-
-//resize
-                  
-        //   const images = document.querySelector('body');
-        //   images.forEach(pics => {
-        //   pics.addEventListener('resize', (e) => {
-        //   pics.style.color = 'orange'})
-        // }); 
-
-//scroll
-
-
-     const scrollin = document.querySelectorAll('.text-content p');
-     scrollin.forEach(pp => {
-   pp.addEventListener('scroll', (e) => {
-    pp.style.color = 'green'})
-       
-   }); 
-
-
-
-//select  
 
 
 
 
-
-// Prevent Default
-
-
-const signupbtns = document.querySelectorAll('.btn');
-signupbtns.forEach(btns => {
-  btns.addEventListener('click', (e) => {
-    console.log(`button click event!`);
-    event.preventDefault();})
- 
-}); 
-
-
-
-       
